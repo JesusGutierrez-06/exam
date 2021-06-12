f9e0e9934433edf50ea6867b8fdefa2f68612d61
JesusGutierrez-06 <jesusgutierrez0106@gmail.com>
2021-06-11 21:51:47 -0400

    segundo parcial

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiTextil.Models;
+
+namespace ApiTextil.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Textil> Textil { get; set; }
+    }
+}
+
---
+++ Controllers/TextilController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ApiTextil.Context;
+
+namespace ApiTextil.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TextilController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public TextilController(AppDbContext _context)
+        {
+            this.context = _context;
+        }
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            //Error handling Try-catch
+            return Ok(context.Textil.ToList());
+        }
+
+    }
+}
